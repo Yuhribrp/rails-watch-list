4bc022376c278d045927cdc5040691fe7b089ea7
YuhriParada <yuhriparada@gmail.com>
2021-08-23 21:23:44 -0300

    add the flash alert

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,6 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates :comment, length: { minimum: 6 }
+  validates :comment, length: { minimum: 8 }
   validates :movie_id, uniqueness: { scope: :list_id, massage: "Already Exists"}
 end
